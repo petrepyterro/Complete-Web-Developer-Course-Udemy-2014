134032ff3c6f438c3b5721e7fbd4911e49646918
petrero <petrero@gmail.com>
2016-11-17 20:09:33 +0200

    Put an image as background. Expand the container on the entire height of window browser with css directives for html, body directives. Without them the container will only have the height of its content(sdf text in our case). Also avoid nasty scaling of the background with css directive background-size:cover

--- public_html/WeatherScraper/index.php
+++ public_html/WeatherScraper/index.php
@@ -15,9 +15,24 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style type="text/css">
+      html, body {
+        height: 100%;
+      }
+      .container {
+        background-image: url('img/background.jpg');
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+      }
+    </style>
   </head>
   <body>
-    
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">sf</div>
+      </div>
+    </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-1.12.4.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
